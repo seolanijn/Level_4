e3d45a105ca625258d27cb734d361146da26a7b8
Seolan <jinseolan929@gmail.com>
2023-01-27 22:27:33 -0500

    Add JavaScript

---
+++ JavaScript/ExercisesAPI/ExercisesAPI/Controllers/MenuItemController.cs
@@ -0,0 +1,27 @@
+﻿using ExercisesAPI.DAL;
+using ExercisesAPI.DAL.DAO;
+using ExercisesAPI.DAL.DomainClasses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace ExercisesAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        readonly AppDbContext _db;
+        public MenuItemController(AppDbContext context)
+        {
+            _db = context;
+        }
+        [HttpGet]
+        [Route("{catid}")]
+        public async Task<ActionResult<List<MenuItem>>> Index(int catid)
+        {
+            MenuItemDAO dao = new(_db);
+            List<MenuItem> itemsForCategory = await dao.GetAllByCategory(catid);
+            return itemsForCategory;
+        }
+    }
+}
---
+++ JavaScript/ExercisesAPI/ExercisesAPI/Helpers/TraySelectionHelper.cs
@@ -0,0 +1,9 @@
+﻿using ExercisesAPI.DAL.DomainClasses;
+namespace ExercisesAPI.Helpers
+{
+    public class TraySelectionHelper
+    {
+        public int Qty { get; set; }
+        public MenuItem? Item { get; set; }
+    }
+}
